c1f78e8fd50ac8ac2e42821887c8da5447a96c6a
EMMANUEL MAZO CABALLERO <EMMANUEL.MAZO@amiria.com.py>
2025-09-23 14:47:33 -0300

    modifica pdf

--- myapp/settings_test.py
+++ myapp/settings_test.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_URL = '/signin'
 
+if not DEBUG:
+    STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
